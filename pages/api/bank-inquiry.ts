98eca9b5aede60cb237987b25c9468b38f2c7250
stanlys96 <stanlyskwok@gmail.com>
2023-08-25 12:34:35 +0700

    wlao

--- pages/api/bank-inquiry.ts
+++ pages/api/bank-inquiry.ts
@@ -1,6 +1,6 @@
 export default function handler(req: any, res: any) {
   if (req.method === "POST") {
-    console.log(req);
+    console.log(req.body);
     res.status(200).json({ status: "success" });
   }
 }
